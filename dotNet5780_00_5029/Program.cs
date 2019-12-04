635caeec0a8887303acd3e15c97f5b643bb6c304
yacov levi <yacovlevi8@outlook.co.il>
2019-12-04 13:11:19 +0200

    Add project files.

---
+++ dotNet5780_00_5029/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5780_00_5029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMethod();
+            Console.ReadKey();
+        }
+
+        private static void WelcomeMethod()
+        {
+            Console.WriteLine("enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, Welcome to my first console application", name);
+        }
+    }
+}
